36585c8d70300ea6e737e5729e8e16495493124c
antoniofs23 <af3036@nyu.edu>
2023-12-15 19:14:14 -0500

    changed str parsing in python rather than bash since its easier

--- pullpower.sh
+++ pullpower.sh
@@ -6,13 +6,3 @@ battery=$(upower -d | grep percentage)
 
 echo $devices
 echo $battery
-# remove repetitions
-# would be easier using xargs sorting but then everything is jumbled up
-#devices=$(echo "$devices" | awk '{for (i=1;i<=NF;i++) if (!devices[$i]++) printf("%s%s",$i,FS)}{printf("\n")}')
-#battery=$(echo "$battery" | awk '{for (i=1;i<=NF;i++) if (!battery[$i]++) printf("%s%s",$i,FS)}{printf("\n")}')
-
-# remove the pesky Diplay device %
-#battery=${battery::-3}
-
-#echo $devices
-#echo $battery
